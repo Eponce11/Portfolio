2b8e6628b05d74d4ac45ed00d023daf022502f13
Eric <ponceeric458@gmail.com>
2023-02-13 08:36:30 -0800

    Projects Component

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Technologies from './components/Technologies'
 import Skills from './components/Skills'
+import Projects from './components/Projects'
 
 function App() {
 
@@ -14,8 +15,10 @@ function App() {
       <div className="flex flex-col justify-center w-[1065px]">
         
         <Home/>
+        <Projects/>
         <Technologies/>
         <Skills/>
+        
       </div>
       
 
---
+++ vite-project/src/components/Projects.tsx
@@ -0,0 +1,29 @@
+
+const Projects = () => {
+
+
+
+
+    return (
+        <div>
+            <h2 className="text-[32px]">Palcom</h2>
+            <div className="flex">
+                <div className="w-[50%]">
+                    <div className="bg-[green] h-[300px]"></div>
+                    <button>Site</button>
+                    <button>Code</button>
+                    <h3 className="text-[20px]">Technologies</h3>
+                </div>
+
+                <div className="w-[50%]"> 
+                    <h3 className="text-[20px]">Description</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ad placeat aliquam ullam nisi explicabo corrupti fuga quas deserunt eius. Maiores asperiores error quia est consequatur, ratione illum quos suscipit?</p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+
+export default Projects

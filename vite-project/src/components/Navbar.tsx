c4fe9b8f15daefdafacc832355df13b89082fac1
Eric Ponce <ponceeric458@gmail.com>
2023-02-14 21:48:13 -0800

    create navbar component

--- vite-project/src/components/Navbar.tsx
+++ vite-project/src/components/Navbar.tsx
@@ -5,8 +5,14 @@
 const Navbar = () => {
 
     return (
-        <div className="bg-[#212529] h-100 w-100">
-            Hello WOrld
+        <div className="flex justify-center items-center bg-[#212529] w-[100%] h-[50px] fixed z-10">
+            <ul className="flex justify-between w-[500px]">
+                <li>Home</li>
+                <li>Skills</li>
+                <li>Technologies</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
         </div>
     )
 
